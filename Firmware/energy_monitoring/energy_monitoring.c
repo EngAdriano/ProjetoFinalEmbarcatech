deafd7b5deb1e2935a571db68000d7c4b32eba11
José Adriano <adriano.liukin@gmail.com>
2025-11-16 11:31:51 -0300

    Reinicio do projeto

--- Firmware/energy_monitoring/energy_monitoring.c
+++ Firmware/energy_monitoring/energy_monitoring.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
 
+#define WIFI_SSID "Lu e Deza"
+#define WIFI_PASSWORD "liukin1208"
 
+volatile bool wifi_connected = false;
 
+// Protótipo de funções
+void vTaskWiFiReconnect(void *pvParameters);
 
 int main()
 {
@@ -19,7 +27,7 @@ int main()
     cyw43_arch_enable_sta_mode();
 
     printf("Connecting to Wi-Fi...\n");
-    if (cyw43_arch_wifi_connect_timeout_ms("Your Wi-Fi SSID", "Your Wi-Fi Password", CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
         printf("failed to connect.\n");
         return 1;
     } else {
@@ -29,8 +37,39 @@ int main()
         printf("IP address %d.%d.%d.%d\n", ip_address[0], ip_address[1], ip_address[2], ip_address[3]);
     }
 
+    // Criação das tarefas FreeRTOS
+    xTaskCreate(vTaskWiFiReconnect, "WiFiReconnect", 512, NULL, 1, NULL);
+
+    // ----- Iniciar scheduler -----
+    vTaskStartScheduler();
+
     while (true) {
         printf("Hello, world!\n");
         sleep_ms(1000);
+    }
+}
+
+
+// ---- Task: reconexão Wi-Fi ----
+void vTaskWiFiReconnect(void *pvParameters) {
+    while (1) {
+        struct netif *netif = &cyw43_state.netif[0];
+        if (ip4_addr_isany_val(netif->ip_addr)) {
+            wifi_connected = false;
+            printf("Wi-Fi desconectado. Tentando reconectar...\n");
+
+            if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD,
+                                                   CYW43_AUTH_WPA2_AES_PSK, 10000)) {
+                printf("Falha na reconexão.\n");
+            } else {
+                wifi_connected = true;
+                ip4_addr_t ip = netif->ip_addr;
+                printf("Reconectado! IP: %s\n", ip4addr_ntoa(&ip));
+            }
+        } else {
+            wifi_connected = true;
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(10000)); // Verifica a cada 10 segundos
     }
 }
